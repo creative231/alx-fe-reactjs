5f3230b331eadd9f025c125cade63f41e4526298
Wendy <wgerrar2002@gmail.com>
2025-11-07 14:23:53 +0000

    MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,6 +2,9 @@ function MainContent() {
     return (
         <main>
             <p>I love to visit New York, Paris, and Tokyo.</p>
+            <main style={{ backgroundColor: '#f0f8ff', padding: '15px', textAlign: 'center', fontSize: '18px' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
         </main>
     );
 }
