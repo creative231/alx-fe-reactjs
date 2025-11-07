f1b108bc3aa97692e249080f82d8a2bd57d946ad
Wendy <wgerrar2002@gmail.com>
2025-11-07 14:12:36 +0000

    Footer.jsx

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -2,6 +2,9 @@ function Footer() {
     return (
         <footer>
             <p>© 2023 City Lovers</p>
+            <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
         </footer>
     );
 }
