93e001ebdae572f8a0969f7e7a2a603a64d14e3e
Wendy <wgerrar2002@gmail.com>
2025-11-12 16:37:47 +0000

    jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,10 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/components/Home";
-import About from "./pages/components/About";
-import Services from "./pages/components/Services";
-import Contact from "./pages/components/Contact";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Services from "./components/pages/Services";
+import Contact from "./components/pages/Contact";
 
 function App() {
   const [count, setCount] = useState(0)
