0c34b511f57a9a909a7d9f2fed65eaee6da43d97
Wendy <wgerrar2002@gmail.com>
2025-11-19 15:02:30 +0000

    jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,7 +1,9 @@
 import { useRecipeStore } from '../recipeStore';
+import { create } from 'zustand';
 
 
-
+const recipes = useRecipeStore((state) => state.recipes);
+console.log(recipes);
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
@@ -17,5 +19,12 @@ const RecipeList = () => {
     </div>
   );
 };
+
 export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
 export default RecipeList;
