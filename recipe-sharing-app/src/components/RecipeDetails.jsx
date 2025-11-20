c4801b899c73012568b7e288d94b2c0ef286171a
Wendy <wgerrar2002@gmail.com>
2025-11-20 08:41:47 +0000

    jsx

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,37 +1,46 @@
-import React from 'react';
+// src/components/RecipeDetails.jsx
 import { useParams, Link } from 'react-router-dom';
 import { useRecipeStore } from '../recipeStore';
-import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
 const RecipeDetails = () => {
   const { id } = useParams();
   const recipeId = Number(id);
-  const recipe = useRecipeStore((s) => s.recipes.find((r) => r.id === recipeId));
-
-  if (!recipe) return (
-    <div style={{ padding: '2rem' }}>
-      <p>Recipe not found.</p>
-      <Link to="/">Back</Link>
-    </div>
+  const recipe = useRecipeStore((s) =>
+    s.recipes.find((r) => r.id === recipeId)
   );
 
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>{recipe.title}</h1>
-      <p>{recipe.description}</p>
-
-      <div style={{ marginTop: '1rem' }}>
-        <DeleteRecipeButton id={recipe.id} />
-      </div>
-
-  <h3 style={{ marginTop: '1.5rem' }}>Edit Recipe</h3>
-  {/* pass key so EditRecipeForm remounts when a different recipe is loaded */}
-  <EditRecipeForm key={recipe.id} recipe={recipe} />
-
-      <div style={{ marginTop: '1rem' }}>
+  if (!recipe) {
+    return (
+      <div>
+        <p>Recipe not found.</p>
         <Link to="/">Back to list</Link>
       </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      {/* Optional extra fields */}
+      {recipe.ingredients && (
+        <>
+          <h4>Ingredients</h4>
+          <ul>
+            {recipe.ingredients.map((ing, i) => (
+              <li key={i}>{ing}</li>
+            ))}
+          </ul>
+        </>
+      )}
+
+      <Link to={`/recipes/${recipe.id}/edit`}>Edit</Link>
+      {' | '}
+      <DeleteRecipeButton recipeId={recipe.id} />
+      <br />
+      <br />
+      <Link to="/">Back to list</Link>
     </div>
   );
 };
