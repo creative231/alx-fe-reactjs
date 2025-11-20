310dcf3b725df6144c312b76931662b3d862db35
Wendy <wgerrar2002@gmail.com>
2025-11-20 10:13:45 +0000

    jsx

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,5 +1,4 @@
 import { useRecipeStore } from './recipeStore';
-import { useNavigate } from 'react-router-dom'
 
 const DeleteRecipeButton = ({ id }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
