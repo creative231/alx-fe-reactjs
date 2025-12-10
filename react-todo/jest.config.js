e3db83c152a92323dea7970d817ec273b491496a
Wendy <wgerrar2002@gmail.com>
2025-12-10 15:24:12 +0000

    jsx

---
+++ react-todo/jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx"],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+};
