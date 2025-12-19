05f3b33bea80a9b1682e8083d41ad7073ff76a6b
Wendy <wgerrar2002@gmail.com>
2025-12-19 12:04:10 +0000

    js

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,7 +8,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import ProfilePage from './components/ProfilePage';
-import UserContext from './components/UserContext';
+import UserContext from './UserContext';
 import UserInfo from './components/UserInfo';
 
 
